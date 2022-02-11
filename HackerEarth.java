3aba43c468c97185f6f211134d3773e50b6aee29
Fahad <xyan4321@gmail.com>
2022-02-11 16:18:15 +0530

    HackerEarth.java added

---
+++ HackerEarth.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.CopyOnWriteArrayList; 
+import java.util.*; 
+class HackerEarth extends Thread
+{ 
+    
+    static CopyOnWriteArrayList hck = new CopyOnWriteArrayList(); 
+  
+    public void testhack() 
+    { 
+        hck.add("K"); 
+    } 
+    
+     public static void main(String[] args) 
+        throws InterruptedException 
+    { 
+        hck.add("H"); 
+        hck.add("A"); 
+        hck.add("C"); 
+    HackerEarth t = new HackerEarth();
+        t.testhack(); 
+  
+        Thread.sleep(1000); 
+        Iterator test1 = hck.iterator(); 
+        while (test1.hasNext()) { 
+            String s = (String)test1.next(); 
+            System.out.println(s); 
+            Thread.sleep(1000); 
+        } 
+        System.out.println(hck); 
+    } 
+}
